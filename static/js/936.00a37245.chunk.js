b595be5ffd0f148430cf0eb3b0bfd46bbdea369e
trangdem99 <dev.trangdem99@gmail.com>
2024-05-18 08:45:48 +0000

    Updates

---
+++ static/js/936.00a37245.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgan_tech=self.webpackChunkgan_tech||[]).push([[936],{9936:(e,t,a)=>{a.r(t),a.d(t,{default:()=>c});var s=a(5043),h=a(579);function c(e){return(0,h.jsx)(s.Fragment,{children:(0,h.jsx)("img",{src:e.src,alt:e.alt,width:e.width,height:e.height,className:e.className})})}}}]);
+//# sourceMappingURL=936.00a37245.chunk.js.map
