2896f97a506871dd2363e8c80d0e7cee8f4063c2
trangdem99 <dev.trangdem99@gmail.com>
2024-05-18 14:17:59 +0700

    chore: Update image path for loader component

--- src/components/loader/index.jsx
+++ src/components/loader/index.jsx
@@ -4,7 +4,7 @@ export default function Loader() {
   return (
     <React.Fragment>
       <div id="global-loader">
-        <img src={process.env.PUBLIC_URL + "/assets/imgages/loader.gif"} alt="Loading..." />
+        <img src={process.env.PUBLIC_URL + "/assets/images/loader.gif"} alt="Loading..." />
       </div>
     </React.Fragment>
   )
